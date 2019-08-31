7c51df176f9d60be36068261b8e2e8e4dcfb1460
Nickan <3498992+Nickan@users.noreply.github.com>
2019-09-01 03:09:09 +0800

    Fixed unable to set the sibling as a parent

--- main/elonComponent.js
+++ main/elonComponent.js
@@ -236,7 +236,7 @@ class ElonComponent {
     function initNearestNodeDetectorCircle(nodeEnter, root, ec) {
       nodeEnter.append("circle")
       .attr('class', 'detectorCircle')
-      .attr("r", 15)
+      .attr("r", 20)
       .attr("opacity", 0.2) // change this to zero to hide the target area
       .style("fill", "red")
       .style("display", "none")
