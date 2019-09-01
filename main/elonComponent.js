159f6e1146de407b59de777617fcb3774afd818b
Nickan <3498992+Nickan@users.noreply.github.com>
2019-09-01 13:21:17 +0800

    Refactored append node

--- main/elonComponent.js
+++ main/elonComponent.js
@@ -63,33 +63,6 @@ class ElonComponent {
       this.update(source, root);
       Event.dispatchEvent(Event.UPDATE_TREE_AFTER, {});
     });
-
-    window.addEventListener(Event.APPEND_NODE, (e) => {
-      appendChild(e.detail.appendTo, e.detail.toAppend);
-      setDescendantsDepth(e.detail.appendTo);
-      Event.dispatchEvent(Event.UPDATE_TREE, {nodeSource: e.detail.appendTo});
-
-      function appendChild(parent, child) {
-        Node.changeParent(child, parent);
-        
-        if (parent.children == undefined) {
-          parent.children = [child];
-        } else {
-          parent.children.push(child);
-        }
-      }
-
-      function setDescendantsDepth(parent) {
-        let c = parent.children;
-        if (c != undefined) {
-          for (let i = 0; i < c.length; i++) {
-            let child = c[i];
-            child.depth = parent.depth + 1;
-            setDescendantsDepth(child);
-          }
-        }
-      }
-    });
   }
 
   update(source, root) {
